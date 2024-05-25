6af011e8a9bb990fd0cb266ad2c66250597dd53f
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-05-25 23:03:46 +0530

    landing page started

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
 import Wrapper from "@/components/Wrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <Wrapper className="bg-green-400">
-    <div>testttttttt</div>
-   </Wrapper>
+    <>
+      <Wrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">DocuChat is now public!</p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-blue-600">documents</span> in seconds.
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">DocuChat allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away.</p>
+        <Link
+          className={buttonVariants({
+            size: "lg",
+            className: "mt-5",
+          })}
+          href="/dashboard"
+          target="_blank">
+          Get started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </Wrapper>
+    </>
   );
 }
