c80b854c4aba6d5b2de1a048ceac18c4c7fb59da
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-05-29 22:27:44 +0530

    prisma sycn up

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,6 +9,8 @@ const Dashboard = () => {
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
         <UploadButton></UploadButton>
       </div>
+
+      
     </main>
   );
 };
