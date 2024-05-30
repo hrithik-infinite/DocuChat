4d408e8f3902c9ed84ed1bf2bf64050209da6c8f
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-05-30 23:45:54 +0530

    started uploadd functionality

--- src/components/PDFRenderer.js
+++ src/components/PDFRenderer.js
@@ -1,8 +1,11 @@
-
 const PDFRenderer = () => {
   return (
-    <div>PDFRenderer</div>
-  )
-}
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">PDFRenderer</div>
+      </div>
+    </div>
+  );
+};
 
-export default PDFRenderer
+export default PDFRenderer;
--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -3,6 +3,30 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+import { Cloud } from "lucide-react";
+
+const UploadDropzone = () => {
+  return (
+    <Dropzone multiple={false}>
+      {({ getRootProps, getInputProps, acceptedFiles }) => {
+        <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
+          <div className="flex items-center justify-center h-full w-full">
+            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span> or drag and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (up to 4 MB)</p>
+              </div>
+            </label>
+          </div>
+        </div>;
+      }}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +42,9 @@ const UploadButton = () => {
       <DialogTrigger onClick={() => setIsOpen(true)} asChild>
         <Button>Upload PDF</Button>
       </DialogTrigger>
-      <DialogContent>Testttttttt</DialogContent>
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 };
