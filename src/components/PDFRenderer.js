6f2bce457c2d62df7bea1c6f2d63ba30401f28c0
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-06-01 17:38:19 +0530

    pdf top bar added

--- src/components/PDFRenderer.js
+++ src/components/PDFRenderer.js
@@ -1,19 +1,78 @@
 "use client";
-import { Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { useToast } from "./ui/use-toast";
 import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
 const PDFRenderer = ({ url }) => {
   const { toast } = useToast();
+  const [numPages, setNumPages] = useState();
+  const [currPage, setCurrPage] = useState(1);
+  const CustomPageValidator = z.object({
+    page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({
+    defaultValues: {
+      page: "1",
+    },
+    resolver: zodResolver(CustomPageValidator),
+  });
   const { width, ref } = useResizeDetector();
+  const handlePageSubmit = ({ page }) => {
+    setCurrPage(Number(page));
+    setValue("page", String(page));
+  };
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5">PDFRenderer</div>
+        <div className="flex items-center gap-1.5">
+          <Button
+            variant="ghost"
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+            }}>
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5">
+            <Input
+              {...register("page")}
+              className={cn("w-12 h-8", errors.page && "focus-visible:ring-red-500")}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit(handlePageSubmit)();
+                }
+              }}
+            />
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+          <Button
+            variant="ghost"
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() => {
+              setCurrPage((prev) => (prev + 1 > numPages ? numPages : prev + 1));
+            }}>
+            <ChevronUp className="h-4 w-4" />
+          </Button>
+        </div>
       </div>
       <div className="flex-1 w-full max-h-screen">
         <div ref={ref}>
@@ -30,10 +89,10 @@ const PDFRenderer = ({ url }) => {
                 variant: "destructive",
               });
             }}
-            // onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
             file={url}
             className="max-h-full">
-            <Page width={width ? width : 1} pageNumber={1}></Page>
+            <Page width={width ? width : 1} pageNumber={currPage}></Page>
           </Document>
         </div>
       </div>
