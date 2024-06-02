aa06d3db28891392e64b4008f395d712a5b7522c
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-06-02 12:37:31 +0530

    added pdf loading states for chat wrapper window

--- src/components/chat/Messages.js
+++ src/components/chat/Messages.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
+import axios from "axios";
 
 const Messages = () => {
+  const msgHandle = async () => {
+    const file = await axios.get(`/api/getFileUploadStatus?id=clwwchk1r0001hsv208wry58`);
+    console.log(file.data.uploadStatus);
+  };
   return (
-    <div>Messages</div>
-  )
-}
+    <div>
+      <Button onClick={msgHandle}>API Call</Button>
+    </div>
+  );
+};
 
-export default Messages
+export default Messages;
