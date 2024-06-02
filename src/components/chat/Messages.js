066bd1ee23574e632f349a245b0cfd42d1f14cf9
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-06-02 23:59:34 +0530

    removed test api btn

--- src/components/chat/Messages.js
+++ src/components/chat/Messages.js
@@ -3,13 +3,10 @@ import { Button } from "../ui/button";
 import axios from "axios";
 
 const Messages = () => {
-  const msgHandle = async () => {
-    const file = await axios.get(`/api/getFileUploadStatus?id=clwwchk1r0001hsv208wry58`);
-    console.log(file.data.uploadStatus);
-  };
+  
   return (
     <div>
-      <Button onClick={msgHandle}>API Call</Button>
+      Messages will appear here
     </div>
   );
 };
