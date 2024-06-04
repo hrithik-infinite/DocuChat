5edf2fadac9c266dfbaf1a88fb29bdd7c054a837
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-06-04 22:11:36 +0530

    completed infinite scrolling

--- src/components/chat/Messages.js
+++ src/components/chat/Messages.js
@@ -1,5 +1,5 @@
 "use client";
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import axios from "axios";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { INFINITE_QUERY_LIMIT, loadingMessage } from "@/lib/utils";
@@ -28,13 +28,22 @@ const Messages = ({ fileId }) => {
   const messages = data?.pages.flatMap((page) => page.messages);
   const combinedMessages = [...(isAiThinking ? [loadingMessage] : []), ...(messages ?? [])];
   const lastMessageRef = useRef(null);
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  });
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((message, i) => {
           const isNextMessageSamePerson = combinedMessages[i - 1]?.isUserMessage === combinedMessages[i]?.isUserMessage;
           if (i === combinedMessages.length - 1) {
-            return <Message ref={lastMessageRef} message={message} isNextMessageSamePerson={isNextMessageSamePerson} key={message.id} />;
+            return <Message ref={ref} message={message} isNextMessageSamePerson={isNextMessageSamePerson} key={message.id} />;
           } else return <Message message={message} isNextMessageSamePerson={isNextMessageSamePerson} key={message.id} />;
         })
       ) : isLoading ? (
