e50906e06c699e5aa22197cb7ae55c382e60fb53
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-06-12 23:47:05 +0530

    aded stripe + testing stripe

--- src/app/api/webhooks/stripe/route.js
+++ src/app/api/webhooks/stripe/route.js
@@ -1,6 +1,8 @@
-import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
 import { headers } from "next/headers";
+import { sub } from "date-fns";
 
 export async function POST(request) {
   const body = await request.text();
@@ -11,46 +13,57 @@ export async function POST(request) {
   try {
     event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET || "");
   } catch (err) {
-    return new Response(`Webhook Error: ${err instanceof Error ? err.message : "Unknown Error"}`, { status: 400 });
+    return NextResponse.json({ error: `Webhook Error: ${err instanceof Error ? err.message : "Unknown Error"}` }, { status: 400 });
   }
+  console.log(event.type, "______________event.type________");
 
   const session = event.data.object;
   if (!session?.metadata?.userId) {
-    return new Response(null, {
-      status: 200,
-    });
+    return NextResponse.json({}, { status: 200 });
+  }
+  switch (event.type) {
+    case "checkout.session.completed":
+      console.log("inside http://localhost:3001/dashboard/billin" , session);
+      await handleCheckoutSessionCompleted(session);
+      break;
+
+    case "invoice.payment_succeeded":
+      await handleInvoicePaymentSucceeded(session);
+      break;
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
   }
 
-  if (event.type === "checkout.session.completed") {
-    const subscription = await stripe.subscriptions.retrieve(session.subscription);
+  return NextResponse.json({}, { status: 200 });
+}
 
-    await prismadb.user.update({
-      where: {
-        id: session.metadata.userId,
-      },
-      data: {
-        stripeSubscriptionId: subscription.id,
-        stripeCustomerId: subscription.customer,
-        stripePriceId: subscription.items.data[0]?.price.id,
-        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
-      },
-    });
-  }
+async function handleCheckoutSessionCompleted(session) {
+  const subscription = await stripe.subscriptions.retrieve(session.subscription);
+console.log("api subs", subscription);
+  await prismadb.user.update({
+    where: {
+      id: session.metadata.userId,
+    },
+    data: {
+      stripeSubscriptionId: subscription.id,
+      stripeCustomerId: subscription.customer,
+      stripePriceId: subscription.items.data[0]?.price.id,
+      stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+    },
+  });
+}
 
-  if (event.type === "invoice.payment_succeeded") {
-    // Retrieve the subscription details from Stripe.
-    const subscription = await stripe.subscriptions.retrieve(session.subscription);
+async function handleInvoicePaymentSucceeded(session) {
+  const subscription = await stripe.subscriptions.retrieve(session.subscription);
 
-    await prismadb.user.update({
-      where: {
-        stripeSubscriptionId: subscription.id,
-      },
-      data: {
-        stripePriceId: subscription.items.data[0]?.price.id,
-        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
-      },
-    });
-  }
-
-  return new Response(null, { status: 200 });
+  await prismadb.user.update({
+    where: {
+      stripeSubscriptionId: subscription.id,
+    },
+    data: {
+      stripePriceId: subscription.items.data[0]?.price.id,
+      stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+    },
+  });
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,11 @@ import Image from "next/image";
 import { buttonVariants } from "./ui/button";
 import { SignInButton, SignUpButton } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
-
+import { auth } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
 const NavBar = () => {
+  const { userId } = auth();
+
   return (
     <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <Wrapper>
@@ -14,30 +17,52 @@ const NavBar = () => {
             <Image src="/DocuChat_logo_black.png" alt="uploading preview" width={180} height={32} quality={100} />
           </Link>
           <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}>
-                Pricing
-              </Link>
-              <SignInButton
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              />
-              <SignUpButton
-                className={buttonVariants({
-                  size: "sm",
-                })}>
-                <button>
-                  Get Started <ArrowRight className="ml-2 h-5 w-5" />
-                </button>
-              </SignUpButton>
-            </>
+            {!userId ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}>
+                  Pricing
+                </Link>
+                <SignInButton
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                />
+                <SignUpButton
+                  className={buttonVariants({
+                    size: "sm",
+                  })}>
+                  <button>
+                    Get Started <ArrowRight className="ml-2 h-5 w-5" />
+                  </button>
+                </SignUpButton>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}>
+                  Dashboard
+                </Link>
+                <Link
+                  href="/dashboard/billing"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}>
+                  Subscription
+                </Link>
+                <UserButton showName />
+              </>
+            )}
           </div>
         </div>
       </Wrapper>
