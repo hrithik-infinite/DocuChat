5f53cfef146e174559187a8ed6a9e7027c661b6e
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-02-20 00:40:53 +0530

    added middleware

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/sign-in",
+        destination: "/api/auth/login",
+        permanent: true
+      },
+      {
+        source: "/sign-up",
+        destination: "/api/auth/register",
+        permanent: true
+      }
+    ];
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
     return config;
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+        search: ""
+      }
+    ]
   }
 };
 
