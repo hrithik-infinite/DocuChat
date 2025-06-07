58f6078e1c13a5d30df4fc8def4e1cb6c0579156
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-06-07 19:53:04 +0530

    Implement file upload functionality with middleware for user authentication

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,24 @@
+import { UploadThingError } from "uploadthing/server";
+import { type FileRouter, createUploadthing } from "uploadthing/next";
+import { currentUser } from "@clerk/nextjs/server";
+
+const f = createUploadthing();
+export const ourFileRouter = {
+  pdfUploader: f({
+    pdf: { maxFileSize: "4MB" }
+  })
+    .middleware(async () => {
+      const user = await currentUser();
+      if (!user) throw new UploadThingError("Unauthorized");
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("upload completed for user id", JSON.stringify(metadata));
+      console.log("file url", file.ufsUrl);
+      return {
+        userId: metadata.userId,
+        file: file
+      };
+    })
+} satisfies FileRouter;
+export type OurFileRouter = typeof ourFileRouter;
--- components/upload/UploadForm.tsx
+++ components/upload/UploadForm.tsx
@@ -1,26 +1,39 @@
 "use client";
+import { useUploadThing } from "@/utils/uploadthing";
 import UploadFormInput from "./UploadFormInput";
 
 import { z } from "zod";
 
-const schema = z.object({
-  file: z
-    .instanceof(File, { message: "Please upload a file." })
-    .refine((file) => file.size <= 5 * 1024 * 1024, { message: "File size must be less than 5 MB." })
-    .refine((file) => file.type === "application/pdf", { message: "File must be a PDF." })
-});
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  const formData = new FormData(e.currentTarget);
-  const file = formData.get("file") as File;
-  const validatedFields = schema.safeParse({ file });
-  if (!validatedFields.success) {
-    console.log(validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalif File");
-    return;
-  }
-  console.log("submitted");
-};
 export default function UploadForm() {
+  const schema = z.object({
+    file: z
+      .instanceof(File, { message: "Please upload a file." })
+      .refine((file) => file.size <= 5 * 1024 * 1024, { message: "File size must be less than 5 MB." })
+      .refine((file) => file.type === "application/pdf", { message: "File must be a PDF." })
+  });
+
+  const { startUpload, routeConfig } = useUploadThing("pdfUploader", {
+    onClientUploadComplete: () => {
+      console.log("Uploaded successfully");
+    },
+    onUploadError: (e) => {
+      console.error(e);
+    },
+    onUploadBegin: () => {}
+  });
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+    const validatedFields = schema.safeParse({ file });
+    if (!validatedFields.success) {
+      console.log(validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalif File");
+      return;
+    }
+    const resp = await startUpload([file]);
+    console.log("submitted", resp);
+    if (!resp) return;
+  };
   return (
     <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
       <UploadFormInput onSubmit={handleSubmit} />
