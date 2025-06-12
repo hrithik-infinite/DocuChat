e7706572bd567f29d9ea8fdd893fc37ee95dbab1
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-06-12 21:12:49 +0530

    feat: enhance DashboardPage with improved layout and upload limit notification; add Card and Dialog components

--- app/(logged-in)/dashboard/page.tsx
+++ app/(logged-in)/dashboard/page.tsx
@@ -1,21 +1,36 @@
 import BgGradient from "@/components/common/bg-gradient";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { ArrowRight, Plus } from "lucide-react";
 import Link from "next/link";
 
 export default function DashboardPage() {
+  const uploadLimit = 5;
   return (
     <main className="min-h-screen">
       <BgGradient />
       <div className="container mx-auto flex flex-col gap-4">
-        <h1>You Summaries</h1>
-        <p>Transform your PDF&apos;s into concise, actionable insights</p>
-        <div>
-          <Button variant={"link"}>
-            <Link href="/upload" className="flex text-white items-center">
-              <Plus className="w-5 h-5 mr-2" />New Summary
-            </Link>
-          </Button>
+        <div className="px-2 py-12 sm:py-24">
+          <div className="flex gap-4 mb-8 justify-between">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-4xl font-bold tracking-tight bg-linear-to-r from-gray-600 to-gray-900 bg-clip-text text-transparent">Your Summaries</h1>
+              <p className="text-gray-600">Transform your PDF&apos;s into concise, actionable insights</p>
+            </div>
+            <Button variant={"link"} className="bg-linear-to-r from-rose-500 to-rose-700 hover:from-rose-700 hover:to-rose-500 hover:scale-105 transition-all duration-300 group hover:no-underline">
+              <Link href="/upload" className="flex text-white items-center">
+                <Plus className="w-5 h-5 mr-2" />
+                New Summary
+              </Link>
+            </Button>
+          </div>
+          <div className="mb-6">
+            <div className="bg-rose-50 border border-rose-200 rounded-lg p-4 text-rose-800">
+              <p className="text-sm">You have reached the limit of 5 uploads on the basic plan. </p>
+              <Link href="/#pricing" className="text-rose-800 underline font-medium underline-offset-4 inline-flex items-center ">
+                Click here to upgrade to Pro <ArrowRight className="w-4 h-4 inline-block" />
+              </Link>{" "}
+              for unlimited uploads.
+            </div>
+          </div>
         </div>
       </div>
     </main>
