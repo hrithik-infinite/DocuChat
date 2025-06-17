f7e41c1ddc4d1f281faacb689fd2f3425af76a9f
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-06-17 22:19:20 +0530

    feat: add maxDuration configuration for serverless functions in Vercel

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -2,6 +2,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
+export const maxDuration = 60;
+
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const user = await currentUser();
   if (!user) {
