6d17a707cc3d743d0b674d988e06432978055ad8
Hrithik Agarwal <hrithikinfinite@gmail.com>
2025-06-17 21:30:41 +0530

    feat: implement route protection in middleware for secure access to protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/summaries(.*)", "/upload(.*)"]);
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
